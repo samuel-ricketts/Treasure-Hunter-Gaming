cbd6f92143b6f9a692aee0018b13cc1bb5c8d27e
Krieger <73131476+AriTAF@users.noreply.github.com>
2022-04-25 16:39:49 -0500

    Door adjustments
    +Added lock functionality
       +Doors that are locked are now not triggers and only become triggers when getting the lock

--- TreasureHuntingUnity/TreasureHuntingUnity/Assets/Scripts/Door.cs
+++ TreasureHuntingUnity/TreasureHuntingUnity/Assets/Scripts/Door.cs
@@ -3,7 +3,7 @@
  * Created on: 4/20/2022
  * 
  * Last Edited By: Krieger
- * Last Edited On: 4/23/2022
+ * Last Edited On: 4/25/2022
  * 
  * Description: Tells the camera what room to enter on colliding with this door
  */
@@ -15,13 +15,21 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
     public Room nextRoom; //holds a reference to the index of the next room
+    public int lockID = -1; //ID of the key needed to open the door, -1 for an unlocked door, 0 for not unlockable
 
     private FollowCam fc;
+    //private Inventory inv;
 
     // Start is called before the first frame update
     void Start()
     {
         fc = FollowCam.FOLLOWCAM;
+        //inv = Inventory.INV;
+
+        if(lockID != 0)
+        {
+            GetComponent<BoxCollider>().isTrigger = false;
+        }
     }
 
     // Update is called once per frame
@@ -39,4 +47,19 @@ public class Door : MonoBehaviour
             fc.ChangeRooms(nextRoom);
         }
     }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject go = collision.gameObject;
+
+        if (go.tag == "Player" && nextRoom != null)
+        {
+            if (lockID == -1 /*|| inv.checkKey(lockID)*/) //if the door has no lock or if the player has the key
+            {
+                GetComponent<BoxCollider>().isTrigger = true;
+                fc.ChangeRooms(nextRoom);
+            }
+        }
+    }
+
 }
